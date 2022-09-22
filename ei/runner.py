abdb7f097f41214425c985a18255e9f4f07bebb9
Alan Boris <39385685+alanb128@users.noreply.github.com>
2022-09-22 01:57:44 -0400

    Update runner.py

--- ei/runner.py
+++ ei/runner.py
@@ -3,14 +3,28 @@ import json
 import datetime
 import time
 import csv
+import os
+import sys, getopt
+import signal
+from edge_impulse_linux.runner import ImpulseRunner
+import io
 
-# TODO: Make these device variables
-save_data_files = True
-upload_data_files = False
-inference_on = False
-line_left = "583e3f17-56de-4926-ab74-22fc4eb6afe8" # line_left
-line_right = "6841ecc6-ccdd-4988-b913-159aab85ab28" # line_right
-sample_interval = 60  # seconds
+# Can be set by device variables or will use default
+if os.getenv('SAVE_DATA_FILES', '1') != '1':
+    save_data_files = False
+else:
+    save_data_files = True
+if os.getenv('UPLOAD_DATA_FILES', '0') != '1':
+    upload_data_files = True
+else:
+    upload_data_files = False
+if os.getenv('INFERENCE_ON', '1') != '1':
+    inference_on = False
+else:
+    inference_on = True
+line_left = os.getenv('LINE_LEFT_ID', '583e3f17-56de-4926-ab74-22fc4eb6afe8') # line_left
+line_right = os.getenv('LINE_RIGHT_ID', '6841ecc6-ccdd-4988-b913-159aab85ab28') # line_right
+sample_interval = int(os.getenv('SAMPLE_INTERVAL', '40'))  # seconds
 
 # Initialize global variables
 recording_count = 1
@@ -117,31 +131,69 @@ def save_rec_data(recording_id):
         if car['duration'] != 0:
             car_count = car_count + 1
             #print("here 3")
-            final.append({'timestamp': int(car['timestamp']), 'journey_dur': int(car['duration']), 'hod': car['hod']} )
+            final.append({'timestamp': int(car['timestamp']), 'journey_dur': int(car['duration']), 'hod': car['hod']})
             #f.write(str(car['timestamp']) + "," + str(car['duration']) + "," + str(car['hod']))
+    print("Found {} car journey(s).".format(car_count))
     if save_data_files:
         nf = "{:02d}{:02d}{:02d}{:02d}{:02d}".format(rec_date.month, rec_date.day, rec_date.hour, rec_date.minute, rec_date.second)
         fname = get_traffic(total_car_count) + ".sample" + nf + ".csv"
+        if car_count == 0:
+            print("NOTE: No cars detected, using default values!")
+            final.append({'timestamp': 1000, 'journey_dur': 1000, 'hod': 0} )
         with open(fname, "w", encoding="UTF8", newline="") as csvf:
             writer = csv.DictWriter(csvf, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(final)
-        if car_count == 0:
-            print("no cars to save!")
-            # empty the features file
-            f = open("features.txt", "w")
-            f.write("")
-            f.close()
-        else:
-            # overwite the features file
-            with open("test/" + fname + ".csv", "w", encoding="UTF8", newline="") as csvf:
-                writer = csv.DictWriter(csvf, fieldnames=fieldnames)
-                writer.writeheader()
-                writer.writerows(final)
-    #print(cars)
-    print("Found {} car journey(s).".format(car_count))
-    print("Saved to file: {}".format(fname))
-    print(final)
+        print("SAVED_DATA_FILE=True; Saved to file: {}".format(fname))
+
+    if inference_on:
+        # convert final to new format - temporary?
+        journey_sum = 0
+        hod = 0
+        for item in final:
+            journey_sum = journey_sum + int(item['journey_dur'])
+            hod = item['hod']  # will only get last value
+        final_mod = [car_count, journey_sum/car_count]
+        print("Final mod: {}".format(final_mod)) 
+        with open("features.txt", "w", encoding="UTF8", newline="") as csvf:
+            writer = csv.writer(csvf)
+            #writer.writeheader()
+            writer.writerow(final_mod)
+        print("INFERENCE_ON=True; Saved features.txt")
+
+def EI_inference():
+    #
+    # Uses local model file to perform inference on collected data
+    #
+
+    print("Starting local EI inference on model...")
+
+    runner = None
+
+    features_file = io.open("/usr/src/app/features.txt", 'r', encoding='utf8')
+    features = features_file.read().strip().split(",")
+    if '0x' in features[0]:
+        features = [float(int(f, 16)) for f in features]
+    else:
+        features = [float(f) for f in features]
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    modelfile = "/usr/src/app/modelfile.eim"
+
+    runner = ImpulseRunner(modelfile)
+    try:
+        model_info = runner.init()
+        print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
+
+        res = runner.classify(features)
+        print("classification:")
+        print(res["result"])
+        print("timing:")
+        print(res["timing"])
+
+    finally:
+        if (runner):
+            runner.stop()
 
 # Start here
 
@@ -153,7 +205,14 @@ while True:
     started = datetime.datetime.now()
     started_formatted = "{:02d}{:02d}{:02d}{:02d}{:02d}".format(started.month, started.day, started.hour, started.minute, started.second)
     print("Started recording file: {}.".format(started_formatted))
-    time.sleep(sample_interval)
+    interval_qtr = sample_interval/4
+    time.sleep(interval_qtr)
+    print("{} s. remaining...".format(interval_qtr * 3), end = " ")
+    time.sleep(interval_qtr)
+    print("{} s. remaining...".format(interval_qtr * 2))
+    time.sleep(interval_qtr)
+    print("{} s. remaining...".format(interval_qtr), end = " ")
+    time.sleep(interval_qtr)
     print("Stopping recording...")
     r = requests.get("http://opendatacam:8080/recording/stop")
     rr = get_last_recording()
@@ -161,7 +220,7 @@ while True:
     
     if inference_on:
         # Call EI inference code here
-        pass
+        EI_inference()
    
     if upload_data_files:
         # Upload files back to EI for training
